6ee0848a8d5aee5d0f3e081f1266e2ca25e76017
Seal <info@sealsecurity.io>
2026-09-07 21:52:50 +0000

    CVE-2026-59877

--- src/parse.js
+++ src/parse.js
@@ -670,6 +670,9 @@ function parse(source, root, options) {
             }
 
             while (token !== "=") {
+                if (token === null) {
+                    throw illegal(token, "end of input");
+                }
                 if (token === "(") {
                     var parensValue = next();
                     skip(")");
